c6d126c0ab60ac6d9ce6cf16397d264bb8ae3a1e
Serhii Fokin <sergiyborysovych@ukr.net>
2023-03-16 07:50:51 +0200

    Constituency Treebank XPOS tags added

--- README.md
+++ README.md
@@ -1,15 +1,23 @@
 This script is aimed at tagging text in a utf-8 encoded .txt file with pos_ner_deprel_head notation. I.e., each token of the text 
-gets assigned underscored tags in the following order: _<PART OF SPEECH>_<NER>_<TYPE OF SYNTACTIC DEPENDENCY>_<DEPENDENCY_HEAD>
+gets assigned underscored tags in the following order: _<PART OF SPEECH>_<NER>_<TYPE OF SYNTACTIC DEPENDENCY>_<TREEBANK_TAG>_<DEPENDENCY_HEAD>
 The tags are assigned as per stanza library: https://stanfordnlp.github.io/stanza/performance.html
+
+This program runs on Python 3.8 or later.
 
 The POS tags are assigned as per UD:
 https://universaldependencies.org/u/pos/
-Dependency relation tags:
-https://universaldependencies.org/docs/u/dep/index.html
+
 NER tags:
 https://github.com/stanfordnlp/stanza/issues/904
+Treebank specific POS tags (XPOS):
+https://www.ling.upenn.edu/courses/Fall_2003/ling001/penn_treebank_pos.html
+Dependency relation tags:
+https://universaldependencies.org/docs/u/dep/index.html
 
-To run the script, use the command
+To run the script, first install the 'stanza' library:
+
+pip install stanza  
+use the command
 
 python synerparser.py <filename> <language>
 
--- synerparser.py
+++ synerparser.py
@@ -14,7 +14,7 @@ def text_to_pos_ner(file_path, language_code):
     print(doc)
     for sentence in doc.to_dict():
         for word in sentence:
-            resulting_text += word["text"] + "_" + word["upos"] + "_" + word["ner"] + "_" + word["deprel"] + "_" + sentence[word["head"]-1]["text"] +  " "
+            resulting_text += word["text"] + "_" + word["upos"] + "_" + word["ner"] + "_" + word["xpos"] + "_" + word["deprel"] + "_" + sentence[word["head"]-1]["text"] +  " "
         resulting_text += "\n"
     return resulting_text
 
@@ -24,19 +24,6 @@ if __name__ == '__main__':
     text_fr_file = open("un_ukr_art2.txt", "r", encoding="utf-8")
     text = text_fr_file.read().replace("\n", "")
     print(text)
-    #stanza.download('uk')
-
-    # doc = nlp(text)
-    # print(doc.to_dict())
-    # print(type(doc.to_dict()))
-    #
-    #
-    # print(type(doc))
-    # print(doc.sentences[1])
-    # print("dependencies")
-    # doc.sentences[1].print_tokens()
-    # doc.sentences[1].print_words()
-
     input_file = open(sys.argv[1]+"_parsed", "w", encoding="utf-8")
     input_file.write(text_to_pos_ner(sys.argv[1], sys.argv[2]))
     # doc.sentences[1].print_dependencies()
