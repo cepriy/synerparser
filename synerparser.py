c57072beb2d68b753306acf6877a3c5d9a7fb95c
Serhii Fokin <sergiyborysovych@ukr.net>
2023-05-02 19:16:30 +0300

    unnecessary output removed

--- synerparser.py
+++ synerparser.py
@@ -7,7 +7,7 @@ def text_to_pos_ner(file_path, language_code):
     text_fr_file = open(file_path, "r", encoding="utf-8")
     text = text_fr_file.read().replace("\n", "")
     doc = nlp(text)
-    print(doc)
+
     for sentence in doc.to_dict():
         for word in sentence:
             if "upos" in word.keys():
@@ -26,11 +26,9 @@ def text_to_pos_ner(file_path, language_code):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    if len(sys.argv):
+    if len(sys.argv) < 2:
         print("Usage:\npython synerparser.py <filename> <language_code>")
-    text_fr_file = open("un_ukr_art2.txt", "r", encoding="utf-8")
-    text = text_fr_file.read().replace("\n", "")
-    print(text)
+
     input_file = open(sys.argv[1]+"_parsed", "w", encoding="utf-8")
     input_file.write(text_to_pos_ner(sys.argv[1], sys.argv[2]))
     # doc.sentences[1].print_dependencies()
